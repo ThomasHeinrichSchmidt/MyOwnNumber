5862e9c76f09411d1e327c964981b280e178f0ab
ThomasHeinrichSchmidt <justmaybe@go4more.de>
2018-10-15 18:19:49 +0200

    fixed failed to post notification (added notification channel)

--- app/src/main/java/de/thschmidt/myownnumber/DisplayNotification.java
+++ app/src/main/java/de/thschmidt/myownnumber/DisplayNotification.java
@@ -9,6 +9,7 @@ package de.thschmidt.myownnumber;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -16,6 +17,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 /**
  * Created by --thomas. in June 2015.
@@ -54,8 +56,23 @@ class DisplayNotification implements Runnable {
             PendingIntent pendingIntent = PendingIntent.getActivity(context,
                     NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
             String msg = "\u2709  \u00ab" + MainActivity.getOwnPhoneNumber(context) + "\u00bb  \u27A0  \u260F " + mCallingNumber;   //  letter arrow phone (U+2709 U+27A0 U260F)
-            // http://stackoverflow.com/questions/13717492/notifications-builder-in-api-10
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+
+            String NOTIFICATION_CHANNEL_ID = "channel_718477";
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {  // due to runtime toast: Developer warning for package "de.thschmidt.myownnumber" - Failed to post notification on channel "null"
+                int importance = NotificationManager.IMPORTANCE_LOW;  // or IMPORTANCE_HIGH; ?
+                CharSequence NOTIFICATION_CHANNEL_NAME = "de.thschmidt.myownnumber_channel_718477";
+                NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance);
+                String Description = "Channel to notify about incoming call and offer to text/SMS the own number to the caller";
+                notificationChannel.setDescription(Description);
+                notificationChannel.enableLights(true);
+                notificationChannel.setLightColor(Color.RED);
+                notificationChannel.enableVibration(true);
+                notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                notificationChannel.setShowBadge(false);
+                mNotificationManager.createNotificationChannel(notificationChannel);
+            }
+                // http://stackoverflow.com/questions/13717492/notifications-builder-in-api-10
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
                     .setContentTitle(context.getString(R.string.MyOwnNumber))
                     // .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))  // needed? does not seem to make a difference   http://stackoverflow.com/questions/28387602/notification-bar-icon-turns-white-in-android-5-lollipop
                     .setContentText(msg)
@@ -63,8 +80,14 @@ class DisplayNotification implements Runnable {
                     .setColor(Color.rgb(132, 165, 39))  // SMS green, #84a527 (https://www.colorcodehex.com/84a527/)
                     .setSmallIcon(getNotificationIcon())
                     .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher));
-            Notification n;
 
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);  // Utility class for constructing synthetic back stacks for cross-task navigation on Android 3.0 and newer.
+            stackBuilder.addParentStack(MainActivity.class);
+            stackBuilder.addNextIntent(intent);
+            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(resultPendingIntent);
+
+            Notification n;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
                 n = builder.build();
             } else {
